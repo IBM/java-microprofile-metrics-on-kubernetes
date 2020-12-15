8d28b675595160e872f919bc32a85b1d38272bcb
Sanjeev Ghimire <gsanjeev7@gmail.com>
2020-12-15 16:20:08 -0600

    change in deployment and services

--- scripts/set-ingress-host.sh
+++ scripts/set-ingress-host.sh
@@ -5,8 +5,8 @@ fi
 cp ../templates/deploy-ingress.template ../manifests/deploy-ingress.yaml
 CLUSTER_NAME=$1
 
-INGRESS_SECRET=$(bx cs cluster-get "$CLUSTER_NAME" | grep -i "ingress secret" | cut -d : -f 2 | tr -d '\n ')
-INGRESS_SUBDOMAIN=$(bx cs cluster-get "$CLUSTER_NAME" | grep -i "ingress subdomain" | cut -d : -f 2 |  tr -d '\n ')
+INGRESS_SECRET=$(ibmcloud ks cluster get -c "$CLUSTER_NAME" | grep -i "ingress secret" | cut -d : -f 2 | tr -d '\n ')
+INGRESS_SUBDOMAIN=$(ibmcloud ks cluster get -c "$CLUSTER_NAME" | grep -i "ingress subdomain" | cut -d : -f 2 |  tr -d '\n ')
 
 sed -i  s/INGRESS_SUBDOMAIN/"$INGRESS_SUBDOMAIN"/g ../manifests/deploy-ingress.yaml
 sed -i  s/INGRESS_SECRET/"$INGRESS_SECRET"/g ../manifests/deploy-ingress.yaml
